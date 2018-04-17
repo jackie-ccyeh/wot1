5ae9ff7684729107d7b1c13d2f2fdc2d9a3a25ee
Jackie <jackie.yeh@amitsys.com>
2018-04-17 22:15:15 +0800

    wot-server1.js add plugin for simulation

--- wot-server1.js
+++ wot-server1.js
@@ -2,6 +2,17 @@
 var httpServer = require('./servers/http'),
 
   resources = require('./resources/model');
+  
+// Internal Plugins
+var ledsPlugin = require('./plugins/internal/ledsPlugin'), //#A
+  pirPlugin = require('./plugins/internal/pirPlugin'), //#A
+  dhtPlugin = require('./plugins/internal/DHT22SensorPlugin'); //#A
+
+// Internal Plugins for sensors/actuators connected to the PI GPIOs
+// If you test this with real sensors do not forget to set simulate to 'false'
+pirPlugin.start({'simulate': true, 'frequency': 2000}); //#B
+ledsPlugin.start({'simulate': true, 'frequency': 10000}); //#B
+dhtPlugin.start({'simulate': true, 'frequency': 10000}); //#B
 
 // HTTP Server
 var server = httpServer.listen(resources.pi.port, function () {
